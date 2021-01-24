2c151611eb96c3e9a1ab8286f253f975163034cb
itcast-z <18210347838@139.com>
2021-01-25 01:22:00 +0800

    ok

--- 2.webpack_study/webpack.config.js
+++ 2.webpack_study/webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 module.exports = {
   // 编译模式
-  mode: 'development' // development  production
+  mode: 'development', // development  production
+  entry: path.join(__dirname, './src/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'), // 输出文件的存放路径
+    filename: 'bundle.js' // 输出文件的名称
+  }
 }
